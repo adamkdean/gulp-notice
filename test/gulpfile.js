ef78ee8ecaa8eb234334d39f12a2bb3d390be1cb
Adam Dean <adam.dean@bravissimo.com>
2015-08-25 13:52:06 +0100

    Changed default notice text

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'),
     notice = require('..');
 
 gulp.task('default', function () {
-    console.log('test');
     gulp.src('fixtures/*.js')
         .pipe(notice())
         .pipe(gulp.dest('output/'));
